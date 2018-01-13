a63e3bbd6a648658db3071eb189d165e8f8000d2
Michael Assink <michael.assink@gmail.com>
2018-01-13 00:31:03 -0600

    Coursera Programming Assignment 2 - Michael Assink

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Michael Assink
+## R Programming Assignment 2 (Coursera)
+## 1/8/2018
 
-## Write a short comment describing this function
+## These functions implement this odd list structure which 
+## embeds functions to enable caching of the results of solve
+## in the exact manner as the functions makeVector and cacheMean 
+## which were provided by the instructors
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## creates this weird fake matrix/function object (returns list)
+  ## that is later passed to cacheSolve
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## takes a CacheMatrix object created by makeCacheMatrix()
+## returns the inverse using makeCacheMatrix$getsolve()
+## implements a cache mechanism which can be inadvertantly overridden
+## by passing a recently instantiated makeCacheMatrix objects
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
